b3bbcbf77afbfab9e66c7a520683c74ecdb6595f
Chihyun Song <ian0371@gmail.com>
2023-09-22 13:14:33 +0900

    Fix errors on lib

--- lib/hardhat/account.ts
+++ lib/hardhat/account.ts
@@ -1,35 +1,41 @@
 import { deployments, ethers } from "hardhat";
 import { hexConcat } from "ethers/lib/utils";
-import { SimpleAccountApiParams, SimpleAccountAPI } from "@account-abstraction/sdk";
+import { SimpleAccountAPI } from "@account-abstraction/sdk";
 
 export async function getContractFromDeployment(name: string) {
   const Dep = await deployments.get(name);
   return await ethers.getContractAt(name, Dep.address);
 }
 
-export interface NonZKGoogleAccountApiParams extends SimpleAccountApiParams {
+export interface NonZKGoogleAccountApiParams {
+  owner: any;
+  provider: any;
+  entryPointAddress: any;
+  salt: number;
   sub: string;
-  recoveryNonce: string;
 }
 export class NonZKGoogleAccountAPI extends SimpleAccountAPI {
+  salt: number;
   sub: string;
-  recoveryNonce: string;
 
-  constructor(params: SimpleAccountApiParams) {
+  constructor(params: NonZKGoogleAccountApiParams) {
     super(params);
+    this.salt = params.salt;
     this.sub = params.sub;
-    this.recoveryNonce = params.recoveryNonce;
   }
 
-  async getAccountInitCode(): Promise<string> {
-    this.factory ??= await ethers.getContractAt("NonZKGoogleAccountFactory", this.factoryAddress);
+  async getAccountInitCode() {
+    // this.factory as any??= ;
+    if (this.factory == null) {
+      this.factory = (await ethers.getContractAt("NonZKGoogleAccountFactory", this.factoryAddress as any)) as any;
+      throw new Error(`Factory is null ${this.factoryAddress}`);
+    }
     return hexConcat([
       this.factory.address,
-      this.factory.interface.encodeFunctionData("createAccount", [
+      (this.factory as any).interface.encodeFunctionData("createAccount", [
         await this.owner.getAddress(),
-        1,
+        this.salt,
         this.sub,
-        this.recoveryNonce,
       ]),
     ]);
   }
